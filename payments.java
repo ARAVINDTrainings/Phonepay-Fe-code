fa9dfcd4cf35b660e2390ad7b2a47c3abe231e82
ARAVIND Trainings <95344915+ARAVINDTrainings@users.noreply.github.com>
2025-03-18 08:20:10 +0530

    Update payments.java

--- payments.java
+++ payments.java
@@ -3,6 +3,7 @@ sudo su -
 yum update -y
 yum install httpd -y
 yum install git -y
+service httpd status
 service httpd start
 chkconfig httpd on
 git clone https://github.com/ARAVINDTrainings/Swiggy.git /var/www/html
